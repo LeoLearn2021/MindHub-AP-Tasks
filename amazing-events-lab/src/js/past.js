8bfd822aea4143f00bb5e76d25869ee1e3a6b1b5
L30lrn <leo.tango.arg@gmail.com>
2023-03-12 23:51:08 -0300

    add dinamic srch ribbon to upcmng- past evnts pgs

--- amazing-events-lab/src/js/past.js
+++ amazing-events-lab/src/js/past.js
@@ -1,10 +1,13 @@
 
 import {renderShowCase,selectEvents, cardShow, getDetailsButtonsListen, renderSelectCategory} from './main'
 import { renderNavigation } from './components/nav-component';
+import { renderSearchRibbon } from './components/ribbon-component';
 
 let { events } = data;
 
 renderNavigation("nav");
+
+renderSearchRibbon("searchRibbon");
 
 renderSelectCategory("catForm");
 
