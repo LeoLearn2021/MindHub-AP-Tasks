80e4672cbc9a565fe02cf36a01a1c085cd1f01f0
L30lrn <leo.tango.arg@gmail.com>
2023-03-16 15:48:19 -0300

    integrate past page with full functionality

--- amazing-events-lab/src/js/past.js
+++ amazing-events-lab/src/js/past.js
@@ -1,16 +1,40 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
-import {renderShowCase, selectEvents, getDetailsButtonsListen, CATEGORIES} from './main'
 import { renderNavigation } from './components/nav-component';
 import { renderSearchRibbon } from './components/ribbon-component';
+import { renderShowCase, updateShow } from './components/render-showcase-component';
+import { selectEvents, getDetailsButtonsListen, searchToSession } from './main';
 
 let { events } = data;
+let searchParams = searchToSession.getSearchParams()
+// searchToSession.setSearchParams(searchParams);
+let pastEvents = selectEvents(
+  events, {
+    upcoming: true,
+    catEvents: searchParams.categorySelection,
+    textSearch: searchParams.findEventText,
+  });
 
 renderNavigation("nav");
+console.log(searchParams);
+sessionStorage.getItem("searchParams") == null ?
+renderShowCase(pastEvents) :
+updateShow(searchParams, pastEvents);
 
-renderSearchRibbon("searchRibbon", CATEGORIES);
-
-let pastEvents = selectEvents(events, {past:true}); 
-renderShowCase(pastEvents, "cardsShow"); // Showing Past events
-console.log(pastEvents.length);
+renderSearchRibbon();
 
 getDetailsButtonsListen('div .card a');
+
+// console.log( JSON.parse(sessionStorage.searchParams) );
+
+// addEventListener("storage", event => {
+//     console.log( JSON.parse(sessionStorage.searchParams) );
+// });
+
+// window.addEventListener("storage", e => {
+//     if(e.storageArea===sessionStorage){
+//         alert('change');
+//       }
+//     console.log( JSON.parse(sessionStorage.searchParams) );
+// });
